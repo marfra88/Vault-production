ab4bb7bc1d7041649c97828bb8c0f7193ac5a1dd
marfra88 <mrc.franchi@gmail.com>
2024-02-15 18:17:13 +0200

    added sensitive to local file

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "tls_private_key" "generated" {
   algorithm = "RSA"
 }
 
-resource "local_file" "private_key_pem" {
+resource "local_sensitive_file" "private_key_pem" {
   content  = tls_private_key.generated.private_key_pem
   filename = "MyAWSKey.pem"
 }
