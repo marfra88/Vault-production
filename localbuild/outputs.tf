1900e302ba09c0d5e7bc7eec02efe6b829db9aea
marfra88 <mrc.franchi@gmail.com>
2024-03-07 09:02:43 +0200

    added private_ip to outputs.tf

--- localbuild/outputs.tf
+++ localbuild/outputs.tf
@@ -4,6 +4,7 @@ output "vault_nodes" {
     {
       instance_id       = instance.id
       public_ip         = instance.public_ip
+      private_ip        = instance.private_ip
       availability_zone = instance.availability_zone
     }
   ]
